8c028cad489f67d295c82099efcfe56cdae06e32
Gujgiczer Anna <gujgicza@users.noreply.github.com>
2017-02-27 07:25:26 -0800

    Added new JUnit test

--- train-system/src/test/java/hu/bme/mit/train/system/test/TrainSystemTest.java
+++ train-system/src/test/java/hu/bme/mit/train/system/test/TrainSystemTest.java
@@ -49,6 +49,11 @@ public class TrainSystemTest {
 		controller.followSpeed();
 		Assert.assertEquals(0, controller.getReferenceSpeed());
 	}
+	
+	@Test
+	public void test3() {
+		Assert.assertEquals(1+1, 2);
+	}
 
 	
 }
