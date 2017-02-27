efff88bd5484b91db7d11af02a56c8cf956f0f7e
daniel-laszlo <laszlodaniel@icloud.com>
2017-02-27 08:34:37 -0800

    tachograph finished and added test

--- train-tachograph/src/main/java/hu/bme/mit/train/user/TrainTachograph.java
+++ train-tachograph/src/main/java/hu/bme/mit/train/user/TrainTachograph.java
@@ -1,12 +1,17 @@
 package hu.bme.mit.train.user;
 
+import com.google.common.collect.HashBasedTable;
 import com.google.common.collect.Table;
 
 public class TrainTachograph {
     Table<Long, Integer, Integer> table;
 
+    public TrainTachograph() {
+        table = HashBasedTable.create();
+    }
+
     public void record(int joyStickPosition, int referenceSpeed) {
-		table.put(System.currentTimeMillis(), joyStickPosition, referenceSpeed);
+        table.put(System.currentTimeMillis(), joyStickPosition, referenceSpeed);
     }
 
     public boolean isTableEmpty() {
