0597dbef579acd416cb9c3e5e14d2e174c295a7b
daniel-laszlo <laszlodaniel@icloud.com>
2017-02-27 08:21:52 -0800

    imported google guava

--- train-tachograph/src/main/java/hu/bme/mit/train/user/TrainTachograph.java
+++ train-tachograph/src/main/java/hu/bme/mit/train/user/TrainTachograph.java
@@ -3,8 +3,15 @@ package hu.bme.mit.train.user;
 import com.google.common.collect.Table;
 
 public class TrainTachograph {
-	Table store
+    Table<Integer, Integer, > table;
 
+    public void record() {
+		table.
 
+    }
 
+    public boolean isTableEmpty() {
+//		return table.isEmpty();
+        return true;
+    }
 }
